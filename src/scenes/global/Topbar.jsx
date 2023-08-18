594e852887d07f91b731a03689097b93d8bea997
aishtomer <91372700+aishtomer@users.noreply.github.com>
2023-08-18 13:44:35 +0530

    Design the topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material";
+import {InputBase} from "@mui/material";
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
@@ -19,10 +19,33 @@ const Topbar = () => {
         <Box 
             display="flex" 
             backgroundColor={colors.primary[400]} 
-            borderRadius="3px">
-
+            borderRadius="3px"
+        >
+            <InputBase sx={{ ml: 2, flex: 1}} placeholder="Search" />
+            <IconButton type="button" sx={{ p: 1 }}>
+                <SearchIcon />
+            </IconButton>
         </Box>
-        <IconButton></IconButton>
+
+        {/* ICONS */}
+        <Box display="flex">
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === 'dark' ? (
+                    <DarkModeOutlinedIcon />
+                ) : (
+                    <LightModeOutlinedIcon />
+                )}
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon />
+            </IconButton>
+        </Box>
     </Box>)
 }
 
